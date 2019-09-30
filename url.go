db98e891fa2e44d5cd969064e89bda9e29e9d400
Eric Paris <eparis@redhat.com>
2019-09-30 11:47:32 -0400

    Initial commit of a library to salt and hash urls

---
+++ url.go
@@ -0,0 +1,134 @@
+package urlhash
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"net"
+	"net/url"
+	"strings"
+)
+
+var (
+	allowedWords   = map[string]bool{}
+	OpenShiftWords = map[string]bool{
+		"kubernetes": true,
+		"k8s":        true,
+		"openshift":  true,
+		"console":    true,
+		"api":        true,
+		"com":        true,
+		"net":        true,
+		"org":        true,
+	}
+)
+
+// Returns the sha256 value of salt+value
+func hash(value string, salt string) string {
+	input := salt + value
+	output := sha256.Sum256([]byte(input))
+	return fmt.Sprintf("%x", output)
+}
+
+// Returns the last len(value) sha256 values of salt+value
+func hashTrunc(value string, salt string) string {
+	hashVal := hash(value, salt)
+	hashLen := len(hashVal)
+	partLen := len(value)
+	if hashLen > partLen {
+		hashVal = hashVal[hashLen-partLen:]
+	}
+	return hashVal
+}
+
+// Returns the len(word) hash of a 'word'.
+// returns the word itself if it is 'allowed'
+func hashWord(word, salt string) string {
+	if _, ok := allowedWords[word]; ok {
+		return word
+	}
+	return hashTrunc(word, salt)
+}
+
+// Returns the hash of salt+IP.
+// always returns 3 bytes for each component of the IP.
+func hashIP(ip string, salt string) string {
+	out := ""
+	parts := strings.Split(ip, ".")
+	for i, part := range parts {
+		if i != 0 {
+			out = out + "."
+		}
+		hashVal := hash(part, salt)
+		hashVal = hashVal[len(hashVal)-3:]
+		out = fmt.Sprintf("%s%s", out, hashVal)
+	}
+	return out
+}
+
+// Break the string on `/`, `.`, and `-`. Individually salt+hash each of those
+// `words`. If all of the 'stuff' before the `/` looks like an IP handle it a little
+// differently.
+func hashString(str, salt string) string {
+	out := ""
+	slashParts := strings.Split(str, "/")
+	for i, slashPart := range slashParts {
+		if i == 0 {
+			// This looks like an IP, so make the hash len==3 instead of
+			// equal to len(word)
+			if net.ParseIP(slashPart) != nil {
+				out = out + hashIP(slashPart, salt)
+				continue
+			}
+		} else {
+			out = out + "/"
+		}
+		dotParts := strings.Split(slashPart, ".")
+		for j, dotPart := range dotParts {
+			if j != 0 {
+				out = out + "."
+			}
+			dashParts := strings.Split(dotPart, "-")
+			for k, word := range dashParts {
+				if k != 0 {
+					out = out + "-"
+				}
+				out = out + hashWord(word, salt)
+			}
+		}
+	}
+	return out
+}
+
+// SetAllowedWords allows you to specify words which will not be hashed. These will
+// instead be returned unchanged.
+func SetAllowedWords(allowed map[string]bool) {
+	allowedWords = allowed
+}
+
+// HashURL takes an url and returns a hash. This hash should be non-trivial to get the
+// original value, but should be stable. So one can compare the output of the hash accross
+// different urls. For example if openshift and com are in the 'AllowedWords' the urls
+// might hash as:
+//    https://this.openshift.com -> https://0a31.openshift.com
+//    https://that.openshift.com -> https://deb4.openshift.com
+//    https://this.that -> https://0a31.deb4
+func HashURL(urlString, salt string) string {
+	out := ""
+	u, err := url.Parse(urlString)
+	if err != nil {
+		return hash(urlString, salt)
+	}
+	if u.Scheme != "" {
+		out = u.Scheme + "://"
+	}
+	if u.Host != "" {
+		out = out + hashString(u.Host, salt)
+		if u.Path != "" {
+			out = out + hashString(u.Path, salt)
+		}
+	} else if u.Path != "" {
+		// If the host was not found, treat the path as the host
+		out = out + hashString(u.Path, salt)
+	}
+	return out
+}
---
+++ url_test.go
@@ -0,0 +1,95 @@
+package urlhash
+
+import "testing"
+
+func TestHash(t *testing.T) {
+	testCases := []struct {
+		url          string
+		expected     string
+		allowedWords map[string]bool
+		salt         string
+	}{
+		{
+			url:      "my.openshift.api.console.customer.com",
+			expected: "05.98ee192d6.479.95d230c.ee8ca6dd.6f3",
+		},
+		{
+			url:      "https://my.openshift.api.console.customer.com",
+			expected: "https://05.98ee192d6.479.95d230c.ee8ca6dd.6f3",
+		},
+		{
+			url:      "https://my.openshift.api.console.customer.com/path/to/something/openshift",
+			expected: "https://05.98ee192d6.479.95d230c.ee8ca6dd.6f3/b5bf/39/ca74813cb/98ee192d6",
+		},
+		{
+			url:      "http://my.openshift.api.console.customer.com",
+			expected: "http://05.98ee192d6.479.95d230c.ee8ca6dd.6f3",
+		},
+		{
+			url:      "tftp://my.openshift.api.console.customer.com",
+			expected: "tftp://05.98ee192d6.479.95d230c.ee8ca6dd.6f3",
+		},
+		{
+			url:      "https://my.openshift.api.console.customer.com",
+			expected: "https://5a.758be91d3.c4c.840c123.d6474651.b02",
+			salt:     "mysalt",
+		},
+		{
+			url:      "https://my.openshift.api.console.customer.com",
+			expected: "https://c0.34432a95c.101.cffa6a3.7f29f533.f45",
+			salt:     "mysalt1",
+		},
+		{
+			url:          "https://my.openshift.api.console.customer.com",
+			expected:     "https://05.openshift.api.console.ee8ca6dd.com",
+			allowedWords: OpenShiftWords,
+		},
+		{
+			url:          "http://my.openshift.api.console.customer.com",
+			expected:     "http://5a.openshift.api.console.d6474651.com",
+			allowedWords: OpenShiftWords,
+			salt:         "mysalt",
+		},
+		{
+			url:          "https://my.openshift-console-ingress.api.console.customer.com",
+			expected:     "https://05.openshift-console-5bd922e.api.console.ee8ca6dd.com",
+			allowedWords: OpenShiftWords,
+		},
+		{
+			url:          "https://my.openshift-console-ingress.api.console.customer.com/path/to/something/openshift",
+			expected:     "https://05.openshift-console-5bd922e.api.console.ee8ca6dd.com/b5bf/39/ca74813cb/openshift",
+			allowedWords: OpenShiftWords,
+		},
+		{
+			url:      "127.0.0.1",
+			expected: "04c.7e9.7e9.b4b",
+		},
+		{
+			url:      "127.0.0.1",
+			expected: "a1f.6a3.6a3.024",
+			salt:     "mysalt",
+		},
+		{
+			url:      "https://127.0.0.1",
+			expected: "https://04c.7e9.7e9.b4b",
+		},
+		{
+			url:      "127.0.0.2",
+			expected: "04c.7e9.7e9.b35",
+		},
+		{
+			url:      "127.0.0.1/path/to/something/openshift",
+			expected: "04c.7e9.7e9.b4b/b5bf/39/ca74813cb/98ee192d6",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.url, func(t *testing.T) {
+			SetAllowedWords(tc.allowedWords)
+			out := HashURL(tc.url, tc.salt)
+			if out != tc.expected {
+				t.Errorf("Got %s expected %s", out, tc.expected)
+			}
+		})
+	}
+}
